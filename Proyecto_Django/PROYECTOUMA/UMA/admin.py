8ea582c28e90806dee3e03787f83f362eeafd1e5
Denilsson Montoya Neyra <dmontoyane@gmail.com>
2021-10-01 23:42:48 -0500

    Revert "Update config.json"
    This reverts commit c79568b62259cdc57bc6fea4599903b77410397d.

--- Proyecto_Django/PROYECTOUMA/UMA/admin.py
+++ Proyecto_Django/PROYECTOUMA/UMA/admin.py
@@ -1,22 +1,21 @@
 from django.contrib import admin
-from PROYECTOUMA.UMA.models import Alumno
 from UMA.models import User
-from UMA.models import Alumno
-# from UMA.models import Categoria
-# from UMA.models import Plato
+from UMA.models import Mesa
+from UMA.models import Categoria
+from UMA.models import Plato
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     pass
 
-@admin.register(Alumno)
+@admin.register(Mesa)
 class MesaAdmin(admin.ModelAdmin):
     pass
 
-# @admin.register(Categoria)
-# class CategoriaaAdmin(admin.ModelAdmin):
-#     pass
+@admin.register(Categoria)
+class CategoriaaAdmin(admin.ModelAdmin):
+    pass
 
-# @admin.register(Plato)
-# class PlatoAdmin(admin.ModelAdmin):
-#     pass
+@admin.register(Plato)
+class PlatoAdmin(admin.ModelAdmin):
+    pass
--- Proyecto_Django/PROYECTOUMA/UMA/models.py
+++ Proyecto_Django/PROYECTOUMA/UMA/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.deletion import CASCADE
-from django.db.models.fields import AutoField
 
 # Create your models here.
 class User(AbstractUser):
@@ -9,26 +8,21 @@ class User(AbstractUser):
     usu_ape=models.CharField(max_length=50)
     usu_tipo=models.CharField(max_length=50)
 
-class Alumno(models.Model):
-    idAlumno = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
-    apellidoPaterno = models.CharField(max_length=50)
-    apellidoMaterno = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    fechaRegistro = models.DateField(auto_now=True)
-    apoderado = models.CharField(max_length=50)
-    direccion = models.CharField(max_length=250)
-    instrumento = models.CharField(max_length=50)
-   
-# class Categoria(models.Model):
-#     categoria_nom = models.CharField(max_length=50)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+class Mesa(models.Model):
+    mesa_nro = models.IntegerField(null=True, blank=True)
+    mesa_cap = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-# class Plato(models.Model):
-#     plato_pre = models.FloatField()
-#     plato_nom = models.CharField(max_length=50)
-#     plato_img = models.ImageField(upload_to="plato_fotos")
-#     categoria = models.ForeignKey(Categoria, on_delete=CASCADE)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+class Categoria(models.Model):
+    categoria_nom = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Plato(models.Model):
+    plato_pre = models.FloatField()
+    plato_nom = models.CharField(max_length=50)
+    plato_img = models.ImageField(upload_to="plato_fotos")
+    categoria = models.ForeignKey(Categoria, on_delete=CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

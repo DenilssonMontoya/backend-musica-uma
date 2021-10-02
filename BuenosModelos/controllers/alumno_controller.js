aa6312e0e2b9ef7d4093a8ef4d069eb0eb82b34f
DiegoSanZar <81200983+DiegoSanZar@users.noreply.github.com>
2021-10-02 12:59:56 -0500

    Update alumno_controller.js

--- BuenosModelos/controllers/alumno_controller.js
+++ BuenosModelos/controllers/alumno_controller.js
@@ -27,6 +27,49 @@ class AlumnoController {
                 })
             })
     }
+
+    static findOne(req, res) {
+        let pk = req.params.id
+        Alumno.findByPk(pk)
+            .then( (data) => {
+                res.send(data)
+            })
+            .catch( (err) => {
+                res.status(404).send({
+                    message: err.message
+                })
+            })
+    }
+
+    static update(req, res) {
+        // PUT product/:id
+        let pk = req.params.id
+        let payload = req.body
+
+        Alumno.update(payload, {where: {id: pk} })
+            .then( (data) => {
+                res.send(data)
+            })
+            .catch( (err) => {
+                res.status(400).send({
+                    message: err.message
+                })
+            })
+
+    }
+
+    static delete(req, res) {
+        let pk = req.params.id
+        Alumno.destroy({ where: {id: pk} })
+            .then( (data) => {
+                res.status(200).send("DELETED")
+            })
+            .catch( (err) => {
+                res.status(400).send({
+                    message: err.message
+                })
+            })
+    }
 }
 
 module.exports = {AlumnoController}

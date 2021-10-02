c79568b62259cdc57bc6fea4599903b77410397d
DiegoSanZar <81200983+DiegoSanZar@users.noreply.github.com>
2021-10-01 23:39:03 -0500

    Update config.json

--- Proyecto_Django/PROYECTOUMA/UMA/admin.py
+++ Proyecto_Django/PROYECTOUMA/UMA/admin.py
@@ -1,21 +1,22 @@
 from django.contrib import admin
+from PROYECTOUMA.UMA.models import Alumno
 from UMA.models import User
-from UMA.models import Mesa
-from UMA.models import Categoria
-from UMA.models import Plato
+from UMA.models import Alumno
+# from UMA.models import Categoria
+# from UMA.models import Plato
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     pass
 
-@admin.register(Mesa)
+@admin.register(Alumno)
 class MesaAdmin(admin.ModelAdmin):
     pass
 
-@admin.register(Categoria)
-class CategoriaaAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(Categoria)
+# class CategoriaaAdmin(admin.ModelAdmin):
+#     pass
 
-@admin.register(Plato)
-class PlatoAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(Plato)
+# class PlatoAdmin(admin.ModelAdmin):
+#     pass

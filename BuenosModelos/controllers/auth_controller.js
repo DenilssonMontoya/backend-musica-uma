aaf304d53ed78d326ea22c5b5097aae10964aa81
Denilsson Montoya Neyra <dmontoyane@gmail.com>
2021-10-02 12:57:01 -0500

    Add JWT user

---
+++ BuenosModelos/controllers/auth_controller.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const {Usuario} = require('../models')
+
+class AuthController {
+
+    static auth(req, res) {
+
+        let secret = process.env.TOKEN_SECRET
+
+        let username = req.body.username
+        let password = req.body.password
+
+        let dbuser = Usuario.findOne({
+            where: {
+                nombreUsuario: username
+            }
+        }).then((data) => {
+
+            if (bcrypt.compareSync(password, data.contrasena)) {
+                let payload = { username: username, role: data.tipoUsuario }
+                const token = jwt.sign(payload, secret, {expiresIn: '1800s'})
+                res.json(token)
+            } else {
+                console.log("contrasenas distintas")
+                res.sendStatus(401)
+            }
+        }).catch((err) => {
+            console.log("usuario no existe")
+            res.sendStatus(401)
+        })
+
+        
+    }
+
+}
+
+module.exports = { AuthController }

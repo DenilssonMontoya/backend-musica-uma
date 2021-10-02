1a0dadbd69eaf9ba2407fa57b7b8e2c6a9e359a8
Denilsson Montoya Neyra <dmontoyane@gmail.com>
2021-10-01 23:46:47 -0500

    Revert "fix:#2"
    This reverts commit 6bcd5a75bac231f09cba388a0708a194a0d1cacd.

--- Proyecto_Django/PROYECTOUMA/UMA/admin.py
+++ Proyecto_Django/PROYECTOUMA/UMA/admin.py
@@ -1,21 +1,3 @@
 from django.contrib import admin
-from UMA.models import User
-from UMA.models import Mesa
-from UMA.models import Categoria
-from UMA.models import Plato
+
 # Register your models here.
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Mesa)
-class MesaAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Categoria)
-class CategoriaaAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Plato)
-class PlatoAdmin(admin.ModelAdmin):
-    pass
